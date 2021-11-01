41ccce808dca11599c7d1d3eb84dab84d404ca0f
Bibil Mathew Chacko <bibil.mathew@mindlabssolutions.com>
2021-11-01 12:44:04 +0530

    first commit

---
+++ src/main/java/com/kattathara15/springuploaddemo/SpringUploadDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kattathara15.springuploaddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUploadDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUploadDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kattathara15/springuploaddemo/SpringUploadDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kattathara15.springuploaddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUploadDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
